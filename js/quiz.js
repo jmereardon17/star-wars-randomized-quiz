f36564f21d9c3f84ccb0ed10958e44ef3462c1fa
Jamie Reardon <decor1878@gmail.com>
2020-01-27 21:38:12 +0000

    Add project quiz data js file

---
+++ js/quiz.js
@@ -0,0 +1,62 @@
+const quiz = [
+  { question: `Who was Obi-Wan's former Master?`, answer: `Qui Gon Jinn`,
+    choices: [`Yoda`, `Qui Gon Jinn`, `Mace Windu`, `Luke Skywalker`]
+  },
+  { question: `What planet did Luke train on to become a Jedi?`, answer: `Dagobah`,
+    choices: [`Naboo`, `Coruscant`, `Tatooine`, `Dagobah`]
+  },
+  { question: `What planet was Anakin born on?`, answer: `Tatooine`,
+    choices: [`Alderaan`, `Tatooine`, `Naboo`, `Coruscant`]
+  },
+  { question: `What is the name of Anakin's Mother?`, answer: `Shmi`,
+    choices: [`Leia`, `Shmi`, `May`, `Emily`]
+  },
+  { question: `Who was Darth Sidious apprentice to?`, answer: `Darth Plagueis`,
+    choices: [`Darth Bane`, `Darth Malak`, `Darth Plagueis`, `Darth Vader`]
+  },
+  { question: `How old was Yoda when he died in Return of the Jedi?`, answer: `900`,
+    choices: [`600`, `700`, `800`, `900`]
+  },
+  { question: `Who was the first Jedi to learn to become a Force Ghost?`, answer: `Qui Gon Jinn`,
+    choices: [`Yoda`, `Obi-Wan`, `Qui Gon Jinn`, `Luke Skywalker`]
+  },
+  { question: `What lightsaber form did Mace Windu create?`, answer: `Vaapad`,
+    choices: [`Shii-Cho`, `Soresu`, `Vaapad`, `Shien`]
+  },
+  { question: `Who was Darth Sidious's first apprentice?`, answer: `Darth Maul`,
+    choices: [`Darth Vader`, `Darth Maul`, `Darth Tyranus`, `Darth Voltres`]
+  },
+  { question: `In Return of the Jedi, who killed Jabba the Hutt?`, answer: `Leia`,
+    choices: [`Luke Skywalker`, `R2D2`, `Han Solo`, `Leia`]
+  },
+  { question: `What name does Obi-wan go by on Tatooine?`, answer: `Ben`,
+    choices: [`Owen`, `Will`, `Ben`, `Dan`]
+  },
+  { question: `Which planet was the Jedi Temple on?`, answer: `Coruscant`,
+    choices: [`Alderaan`, `Naboo`, `Coruscant`, `Quesh`]
+  },
+  { question: `What relation is Leia to Luke?`, answer: `Sister`,
+    choices: [`Cousin`, `Sister`, `Mother`, `Friend`]
+  },
+  { question: `What species is Jar Jar Binks?`, answer: `Gungan`,
+    choices: [`Twilek`, `Gungan`, `Neimoidian`, `Rodian`]
+  },
+  { question: `Who created C3P0?`, answer: `Anakin`,
+    choices: [`Shmi`, `Luke`, `Watto`, `Anakin`]
+  },
+  { question: `Who is Kylo Ren's father?`, answer: `Han Solo`,
+    choices: [`Anakin Skywalker`, `Luke Skywalker`, `Han Solo`, `Obi-Wan`]
+  },
+  { question: `What is Kylo Ren's real name?`, answer: `Ben Solo`,
+    choices: [`Ben Skywalker`, `Ben Kenobi`, `Ben Solo`]
+  },
+  { question: `What planet did Leia grow up on?`, answer: `Alderaan`,
+    choices: [`Tatooine`, `Naboo`, `Alderaan`, `Coruscant`]
+  },
+  { question: `What planet do Ewoks live on?`, answer: `Endor`,
+    choices: [`Dagobah`, `Yavin`, `Endor`, `Hoth`]
+  },
+  { question: `Who created Anakin Skywalker?`, answer: `Darth Plagueis`,
+    choices: [`Darth Sidious`, `Darth Plagueis`, `Darth Tenebrous`, `Darth Bane`]
+  }
+];
